c284fe13bc8ed7030d73a563bf409162fcf06f28
Abhinav Singh <abhinav.singh@specadel.com>
2015-02-05 15:58:07 +0530

    Added first 5 map reduce problems

---
+++ session2_mapreduce/problem_2/mapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+
+def read_input(file):
+    for line in file:
+        yield line.split()
+
+
+def main(separator='\t'):
+    data = read_input(sys.stdin)
+    for words in data:
+        for word in words:
+            print '%s%s%s' % (''.join(sorted(word)), separator, word)
+
+if __name__ == "__main__":
+    main()
---
+++ session2_mapreduce/problem_3/mapper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+
+
+def read_input(file):
+    for line in file:
+        yield line.split()
+
+
+def main(separator='\t'):
+    data = read_input(sys.stdin)
+    for user, dna in data:
+        print '%s%s%s' % (dna, separator, user)
+
+if __name__ == "__main__":
+    main()
